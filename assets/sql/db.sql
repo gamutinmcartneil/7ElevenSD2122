4cf15ca620c51945fffb4b9edb68fcbb673ffd61
gamutinmcartneil <gamutinmcartneil@gmail.com>
2022-03-08 18:42:30 +0800

    Creating a Dashboard Page with assets

--- assets/sql/db.sql
+++ assets/sql/db.sql
@@ -6,3 +6,13 @@ CREATE TABLE users(
     users_bday DATE NOT NULL,
     users_created DATETIME not null
 )
+
+CREATE TABLE users(
+    users_id int(11) AUTO_INCREMENT PRIMARY KEY not null,
+    users_name TINYTEXT not null,
+    users_position TINYTEXT not null,
+    users_office LONGTEXT not null,
+    users_age DATE NOT NULL,
+    users_start_date DATETIME not null
+    
+)
